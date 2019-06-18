bfef5c43d6ef9d9cc9fea5aeaaecaa0570e93a5c
Meghan <meghan.avolio@gmail.com>
2019-06-18 13:01:38 -0400

    new folder

---
+++ PhD/ordinations.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+theme_set(theme_bw(12))
+setwd("C:\\Users\\megha\\Dropbox\\Konza Research\\Removal Plots_2011\\PhD_Removals")
+
+stems00<-read.csv("removal study_stems_biomass_00_by growth.csv")
+
